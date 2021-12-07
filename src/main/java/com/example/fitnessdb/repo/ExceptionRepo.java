0fcc870a1a825cb9d4effeb623ba826daf9dc98f
Radosveta Petrova <r_petrova_90@abv.bg>
2021-12-07 09:00:33 +0200

    removed unused entity

--- src/main/java/com/example/fitnessdb/repo/ExceptionRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.fitnessdb.repo;
-
-import com.example.fitnessdb.model.entity.ExceptionEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExceptionRepo extends JpaRepository<ExceptionEntity, Long> {
-}
--- src/main/java/com/example/fitnessdb/service/ExceptionService.java
+++
@@ -1,7 +0,0 @@
-package com.example.fitnessdb.service;
-
-import javax.servlet.http.HttpServletRequest;
-
-public interface ExceptionService {
-    void onRequest(HttpServletRequest request);
-}
